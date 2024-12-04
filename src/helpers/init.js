17d8a409dfaf00f80fd1388f215163f48eab6121
adagio-88 <85228233+adagio-88@users.noreply.github.com>
2024-12-04 14:16:56 +0800

    更新 init.js

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -4,17 +4,17 @@ export function initializeParams(request, env) {
     const proxyIPs = env.PROXYIP?.split(',').map(proxyIP => proxyIP.trim());
     const url = new URL(request.url);
     const searchParams = new URLSearchParams(url.search);
-    globalThis.panelVersion = '2.7.8';
-    globalThis.defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
-    globalThis.defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
-    globalThis.userID = env.UUID|| '9bbc81d6-a764-4191-bea0-7c9b24a3013e';
-    globalThis.trojanPassword = env.TROJAN_PASS|| 'wj252310';
-    globalThis.proxyIP = proxyIPs ? proxyIPs[Math.floor(Math.random() * proxyIPs.length)] : 'bpb.yousef.isegaro.com';
-    globalThis.hostName = request.headers.get('Host');
-    globalThis.pathName = url.pathname;
-    globalThis.client = searchParams.get('app');
-    globalThis.urlOrigin = url.origin;
-    globalThis.dohURL = env.DOH_URL || 'https://cloudflare-dns.com/dns-query';
+    panelVersion = '2.7.8';
+    defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
+    defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
+    userID = env.UUID|| '9bbc81d6-a764-4191-bea0-7c9b24a3013e';
+    trojanPassword = env.TROJAN_PASS|| 'wj252310';
+    proxyIP = proxyIPs ? proxyIPs[Math.floor(Math.random() * proxyIPs.length)] : 'bpb.yousef.isegaro.com';
+    hostName = request.headers.get('Host');
+    pathName = url.pathname;
+    client = searchParams.get('app');
+    urlOrigin = url.origin;
+    dohURL = env.DOH_URL || 'https://cloudflare-dns.com/dns-query';
     if (pathName !== '/secrets') {
         if (typeof env.bpb !== 'object') throw new Error('KV Dataset is not properly set! Please refer to tutorials.', { cause: "init"});
         if (!userID || !trojanPassword) throw new Error(`Please set UUID and Trojan password first. Please go to 🟢 https://${hostName}/secrets 🟢 to generate them.`, { cause: "init"});
